0070c229c05a0fd35771a8d55205e4c10695a409
yas-say <yasowant@live.com>
2023-03-22 18:24:10 -0400

    grid placement

--- main.py
+++ main.py
@@ -13,16 +13,19 @@ window.title("My window")
 window.minsize(width=500, height=600)
 
 my_label =tkinter.Label(text="Here goes my label", font=("Arial", 25))
-my_label.pack()
+my_label.grid(column=0,row=0)
 #my_label["text"] = " New label via dict"
 #my_label.config(text="New label via config")
 
 
 button = tkinter.Button(text  = "click me", command=button_clicked)
-button.pack()
+button.grid(column=1,row=1)
+
+newbutton = tkinter.Button(text  = "new button", command=button_clicked)
+newbutton.grid(column=2,row=0)
 
 input_field = tkinter.Entry()
-input_field.pack()
+input_field.grid(column=3,row=2)
 
 
 
