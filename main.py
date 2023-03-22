69a8f5568051e6545e640ca175be6027b51cdb65
yas-say <yasowant@live.com>
2023-03-22 17:34:41 -0400

    label

--- main.py
+++ main.py
@@ -5,5 +5,7 @@ window = tkinter.Tk()
 window.title("My window")
 window.minsize(width=500, height=600)
 
+my_label =tkinter.Label(text="Here goes my label", font=("Arial", 25))
+my_label.pack()
 
 window.mainloop()
